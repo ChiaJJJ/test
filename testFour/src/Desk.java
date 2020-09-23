cb8965b311853fd18c603863053311d3c4374129
Chia <763032666@qq.com>
2020-09-23 15:35:32 +0800

    桌面版github提交测试

---
+++ testFour/src/Desk.java
@@ -0,0 +1,12 @@
+/**
+ * @author jzz
+ * @Title: Desk
+ * @Description:
+ * @Version 1.0
+ * @date 2020/9/23 15:34
+ */
+public class Desk {
+    public static void main(String[] args) {
+        System.out.println("桌面版github提交测试");
+    }
+}
