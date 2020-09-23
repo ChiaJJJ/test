37042d64029bd808b8f16d6dd0d9ab38348a6344
Chia <763032666@qq.com>
2020-09-23 15:23:56 +0800

    用idea测试提交

---
+++ testFour/src/Test.java
@@ -0,0 +1,12 @@
+/**
+ * @author jzz
+ * @Title: Test
+ * @Description: 测试idea中添加
+ * @Version 1.0
+ * @date 2020/9/23 15:21
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("用idea测试提交");
+    }
+}
